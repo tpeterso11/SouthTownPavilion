07f53a81488b85881558733515d6569e17f5a957
Toussaint <Toussaint.Peterson@icloud.com>
2014-10-18 10:56:41 -0500

    All the rest

---
+++ EventPosts.php
@@ -0,0 +1,25 @@
+<?php
+
+$eventPost = array();
+$eventPost[0] = array(
+"name" => "Boogie Night",
+"date" => "October 2014",
+"background" => "Images/test-back.png",
+"event-info" => "Lorem ipsum dolor sit amet, integre detracto sit ad, sint prompta principes in sed. Aeque soluta constituam cu mea. Est phaedrum honestatis te, enim admodum alienum ut vix. Appetere singulis ius ut, vel sint invenire persecuti ei. Ne pri exerci inermis, vel perfecto voluptatibus ei, an case deseruisse theophrastus nam..."
+);
+$eventPost[1] = array(
+"name" => "White Party",
+"date" => "October 2014",
+"background" => "Images/placeholder_2.jpg",
+"event-info" => "An persius vivendo mei, nam in posse regione torquatos. Legere facilis eum an. Sea in illud inani, ceteros necessitatibus an vel, mei no probatus perpetua adolescens. Assum legimus est ut, et qui quas habemus nostrum. Meis laudem theophrastus ei pro, ut meliore urbanitas assentior quo, tritani voluptua ex per."
+);
+$eventPost[2] = array(
+"name" => "Test 2",
+"date" => "Whenever",
+"event-info" => "Lorem ipsum dolor sit amet, integre detracto sit ad, sint prompta principes in sed. Aeque soluta constituam cu mea. Est phaedrum honestatis te, enim admodum alienum ut vix. Appetere singulis ius ut, vel sint invenire persecuti ei. Ne pri exerci inermis, vel perfecto voluptatibus ei, an case deseruisse theophrastus nam..."
+);
+$eventPost[3] = array(
+"name" => "Test 3",
+"date" => "Whenever",
+"event-info" => "Lorem ipsum dolor sit amet, integre detracto sit ad, sint prompta principes in sed. Aeque soluta constituam cu mea. Est phaedrum honestatis te, enim admodum alienum ut vix. Appetere singulis ius ut, vel sint invenire persecuti ei. Ne pri exerci inermis, vel perfecto voluptatibus ei, an case deseruisse theophrastus nam..."
+);
---
+++ Untitled-4.php
@@ -0,0 +1,163 @@
+<?php $pageTitle = 'index'; ?>
+<?php include ('EventPosts.php'); ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SouthTown Pavilion: Kansas City</title>
+<link href="CSS/index-style.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="CSS/normalize.css" />
+<link rel="stylesheet" type="text/css" href="CSS/component.css" />
+<link rel="stylesheet" type="text/css" href="CSS/demo.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="js/modernizr.custom.js"></script>
+<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+<script src="JS/jquery.easing.1.3.js"></script>
+<script src="JS/jquery.vide.min.js"></script>
+<script src="JS/calendar.js"></script>
+<script>
+$(document).ready(function(e) {
+	var init = function(){
+		$("#nav-button").fadeOut(400, 'linear');
+		$(".container").fadeOut(400, 'linear');
+		$(".new-page").fadeOut(400, 'linear');
+		
+		setTimeout(function(){
+		$("#navigation").slideDown(400, 'linear');
+		$("#top").slideDown(600, 'linear');
+		$("#bottom").slideDown(600, 'linear');
+		$("#overlay").fadeIn(1500, 'linear');
+		$("#div-one").fadeIn(1800, 'linear');
+	}, 800);
+	
+	};
+	var closeinit = function(){
+		$("#navigation").slideUp(400, 'linear');
+		$("#top").slideUp(400, 'linear');
+		$("#bottom").slideUp(400, 'linear');
+		$("#overlay").fadeOut(1500, 'linear');
+		
+		setTimeout(function(){
+			$("#nav-button").fadeIn(400, 'linear');
+		}, 800);
+	};
+	var pageinit = function(){
+		$("#overlay").fadeOut(500, 'linear');
+		$("#div-one").fadeOut(800, 'linear');
+		
+		setTimeout(function(){
+			closeinit();
+		}, 800);
+	};
+    
+	$("#navigation").hide();
+	$("#top").hide();
+	$("#bottom").hide();
+	$("#about-page").hide();
+	$(".container").hide();
+	$("#calendar").hide();
+	$("#nav-button").on('click', function(){
+		init();
+	});
+	$("#x-button").on('click', function(){
+		closeinit();
+	});
+	$("#about").on('click', function(){
+		pageinit();
+		
+		setTimeout(function(){
+			$("#about-page").fadeIn(300, 'linear');
+		}, 2000);
+	});
+	$("#event").on('click', function(){
+		pageinit();
+		
+		setTimeout(function(){
+			$("#calendar").fadeIn(300, 'linear');
+		}, 2000);
+	});
+	$("#contact-us").on('click', function(){
+		pageinit();
+		
+		setTimeout(function(){
+			$(".container").fadeIn(300, 'linear');
+		}, 2000);
+	});
+});
+</script>
+<style>
+        html, body {
+            margin: 0 auto;
+            width: 100%;
+            height: 100%;
+			position: fixed;
+		}
+		.fs-title{
+			color: #000 !important;
+		}
+		.fs-title h1{
+			text-decoration: underline;
+			font-family: second;
+		}
+		.fs-field-label{
+			color: #000 !important;
+		}
+    </style>
+</head>
+
+<body data-vide-bg="video/Record">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../libs/jquery/jquery-1.11.1.min.js"><\/script>')</script>
+<script src="../src/jquery.vide.js"></script>
+<div id="top">
+</div>
+<span id="nav-button">
+<img id="nav-button-1" src="Images/single-bar.png">
+<br><img id="nav-button-2" src="Images/single-bar.png">
+<br><img id="nav-button-3" src="Images/single-bar.png">
+</span>
+<div id="navigation">
+<ul id="nav">
+<li><img class="icon" id="about" src="Images/i-icon.png"></li>
+<li><img class="icon" id="event" src="Images/event-icon.png"></li>
+<li><img class="icon" id="gallery" src="Images/camera-icon.png"></li>
+<li><img class="icon" id="book" src="Images/book-icon.png"></li>
+<li><img class="icon" id="contact-us" src="Images/contact.png"></li>
+<li><span id="x-button"><img class="x" id="x-left" src="Images/single-bar-white.png">
+<img class="x" id="x-right" src="Images/single-bar-white.png"></span></li>
+</ul>
+</div>
+<div id="overlay"></div>
+<div id="div-one">
+<img id="main-logo" src="Images/placeholder_2.jpg">
+</div>
+<?php include ("about.php"); ?>
+<?php include ("calendar.php"); ?>
+<?php include ("contact.php"); ?>
+<div id="bottom">
+</div>
+<script src="JS/classie.js"></script>
+		<script src="JS/selectFx.js"></script>
+		<script src="JS/fullscreenForm.js"></script>
+		<script>
+			(function() {
+				var formWrap = document.getElementById( 'fs-form-wrap' );
+
+				[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {	
+					new SelectFx( el, {
+						stickyPlaceholder: false,
+						onChange: function(val){
+							document.querySelector('span.cs-placeholder').style.backgroundColor = val;
+						}
+					});
+				} );
+
+				new FForm( formWrap, {
+					onReview : function() {
+						classie.add( document.body, 'overview' ); // for demo purposes only
+					}
+				} );
+			})();
+</script>
+</body>
+</html>
