a19ed54bce3f9224031b85bfaa4242826ecca7e8
Toussaint <Toussaint.Peterson@icloud.com>
2014-10-18 10:49:05 -0500

    First Commit

---
+++ contact.php
@@ -0,0 +1,29 @@
+<div class="container">
+	<div class="fs-form-wrap" id="fs-form-wrap">
+        <div class="fs-title">
+            <h1>Contact SouthTown Pavilion</h1>
+        </div>
+<form id="myform" class="fs-form fs-form-full" autocomplete="off">
+	<ol class="fs-fields">
+		<li>
+			<label class="fs-field-label fs-anim-upper" for="q1" data-info="Insert Name here...">What's your name?</label>
+			<input class="fs-anim-lower" id="q1" name="q1" type="text" placeholder="Name" required/>
+		</li>
+		<li>
+			<label class="fs-field-label fs-anim-upper" for="q2" data-info="We won't send you spam, we promise...">What's your email address?</label>
+			<input class="fs-anim-lower" id="q2" name="q2" type="email" placeholder="dean@road.us" required/>
+		</li>
+        <li>
+			<label class="fs-field-label fs-anim-upper" for="q3" data-info="We won't send you spam, we promise...">What's your phone number?</label>
+			<input class="fs-anim-lower" id="q3" name="q3" type="text" placeholder="555-555-5555" required/>
+		</li>
+		<li>
+			<label class="fs-field-label fs-anim-upper" for="q4">What's on your mind?</label>
+			<textarea class="fs-anim-lower" id="q4" name="q4" placeholder="Describe here"></textarea>
+		</li>
+		</li>
+</ol>
+			<button class="fs-submit" type="submit">Send answers</button>
+</form><!-- /fs-form -->
+</div><!-- /fs-form-wrap -->
+</div><!-- /container -->
